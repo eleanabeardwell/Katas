00ac973f6db19bb0927cb052ddb54d21d7b84c3f
Eleana Beardwell <eleana.beardwell@tesco.com>
2019-08-21 10:10:11 +0100

    Updated Poker Kata Solution

--- katapoker/app.js
+++ katapoker/app.js
@@ -1,26 +1,219 @@
 function PokerGame() {
     var deal = (A,B) => {
 
-        numberListA = []
-        numberListB = []
+        var numListA = convertToIntegers(A)
+        var numListB = convertToIntegers(B)
 
+        if(isStraightFlush(A, numListA) || isStraightFlush(B, numListB)) {
+            return calculateStraightFlush(A, B, numListA,numListB)
+        }
+
+        if(isFourOfKind(numListA) || isFourOfKind(numListB)) {
+            return calculateFourOfKind(numListA,numListB)
+        }
+        if(isFullHouse(numListA) || isFullHouse(numListB)) {
+            return calculateFullHouse(numListA,numListB)
+        }
+        if(isFlush(A) || isFlush(B)) {
+            return calculateFlush(A,B)
+        }
+        if(isStraight(numListA) || isStraight(numListB)) { 
+            return calculateStraight(numListA, numListB)
+        }
+        if(isTriple(numListA) || isTriple(numListB)) {   
+            return calculateTriple(numListA, numListB)
+        }
+        if (isTwoPair(numListA) || isTwoPair(numListB)) {   
+            return calculateTwoPair(numListA, numListB)
+        } 
+        if (isPair(numListA) || isPair(numListB)) {    
+            return calculatePair(numListA, numListB)
+        }
+        else {
+            return highCard(numListA, numListB)
+        }             
+    }
+
+    //Straight Flush
+    var calculateStraightFlush = (A,B, numListA, numListB) => {
+        return (isStraightFlush(A, numListA) && isStraightFlush(B, numListA) ? highCard(numListA,numListB) : isStraightFlush(A, numListA) ? "Player A Wins" : "Player B Wins")
+    }
+
+    var isStraightFlush = (A, numListA) => {
+        return isFlush(A) && isStraight(numListA)
+    }
+
+    //Four Of A Kind
+    var calculateFourOfKind = (A,B) => {
+        if (isFourOfKind(A) && isFourOfKind(B)) {
+            return (findFourOfKind(A)[0]>findFourOfKind(B)[0] ? "Player A Wins" : "Player B Wins")
+        } else {
+            return (findFourOfKind(A) ? "Player A Wins" : "Player B Wins")
+        }
+    }
+
+    var findFourOfKind = (A) => {
+        var four = []
+        for (i=0; i<A.length-3; i++) {
+            if (A[i]===A[i+1] && A[i]===A[i+2] && A[i]===A[i+3]) {
+                four.push(A[i])
+                }
+        }
+        return four
+    }
+
+    var isFourOfKind = (numList) => {
+        return findFourOfKind(numList).length===1 
+    }
+
+
+    //Full House
+    var calculateFullHouse = (A,B) => {
+        if (isFullHouse(A) && isFullHouse(B)) {
+            return (findTriple(A)[0]>findTriple(B)[0] ? "Player A Wins" : "Player B Wins")
+        } else {
+            return (isFullHouse(A) ? "Player A Wins" : "Player B Wins")
+        }
+    }
+
+    var isFullHouse = (A) => {
+        if (isTriple(A)) {
+            var remaining = A.filter(num => num!==findTriple(A)[0])
+            return isPair(remaining)
+        } else {
+            return false
+        }
+    }
+    //Flush
+    var calculateFlush = (A,B) => {
+        var numListA = convertToIntegers(A)
+        var numListB = convertToIntegers(B)
+         return (isFlush(A) && isFlush(B) ? highCard(numListA,numListB) : isFlush(A) ? "Player A Wins" : "Player B Wins")
+    }
+
+    var isFlush = (A) => {
+        const allSuitsEqual = arr => arr.every(v => v === arr[0])
+        return allSuitsEqual(convertToSuits(A))
+
+    }
+
+    //Straight
+    var calculateStraight = (A,B) => {
+        return (isStraight(A) && isStraight(B) ? highCard(A,B) : isStraight(A) ? "Player A Wins" : "Player B Wins")
+    }
+
+    var isStraight = (A) => {
+        for (i=0; i<A.length-1; i++) {
+            if (A[i+1] - A[i] !== 1) {
+                return false
+            } 
+        } return true
+    }
+
+    //Triple
+    var calculateTriple = (A,B) => {
+        if (findTriple(A).length===1 && findTriple(B).length===1) {
+            return (findTriple(A)[0]>findTriple(B)[0] ? "Player A Wins" : "Player B Wins")
+        } else {
+            return (findTriple(A).length===1 ? "Player A Wins" : "Player B Wins")
+        }
+    }
+
+    var findTriple = (A) => {
+        var triple = []
+        for (i=0; i<A.length-2; i++) {
+            if (A[i]===A[i+1] && A[i]===A[i+2]) {
+                triple.push(A[i])
+                }
+        }
+        return triple
+    }
+
+    var isTriple = (numList) => {
+        return findTriple(numList).length===1 
+    }
+
+    //Double Pair
+    var calculateTwoPair = (A,B) => {
+        if (findPairs(A).length===2 && findPairs(B).length===2) {
+            var remainingA = A.filter(num => num!==findPairs(A)[0] && num!==findPairs(A)[1])
+            var remainingB = B.filter(num => num!==findPairs(B)[0] && num!==findPairs(B)[1])
+            return (checkSamePairs(findPairs(A),findPairs(B)) ? highCard(remainingA, remainingB) : A[1]>B[1] ? "Player A Wins" : "Player B Wins")
+        } 
+        else {
+            return (findPairs(A).length===2 ? "Player A Wins" : "Player B Wins")
+        }
+    }
+
+    var isTwoPair = (numList) => {
+        return findPairs(numList).length===2
+    }
+
+    // Pair
+    var calculatePair = (A,B) => {
+        if (findPairs(A).length===1 && findPairs(B).length===1) {
+            var remainingA = A.filter(num => num!==findPairs(A)[0])
+            var remainingB = B.filter(num => num!==findPairs(B)[0])
+            return (findPairs(A)[0]===findPairs(B)[0] ? highCard(remainingA, remainingB) : findPairs(A)[0]>findPairs(B)[0] ? "Player A Wins" : "Player B Wins")
+        } 
+        else {
+            return (findPairs(A).length===1 ? "Player A Wins" : "Player B Wins")
+        }
+    }
+
+    var findPairs = (A) => {
+        var pair = []
+        for (i=0; i<A.length-1; i++) {
+            if (A[i]===A[i+1]) {
+                pair.push(A[i])
+            }
+        } 
+        return pair
+    }
+
+    var isPair = (numList) => {
+        return findPairs(numList).length===1
+    }
+
+    var highCard = (A,B) => {
+
+        while(A.length >= 0) {
+            var maxNumA = Math.max(...A)
+            var maxNumB = Math.max(...B)
+            if (A.length===0) {
+                return "Draw"
+            }
+            if (maxNumA===maxNumB) {
+                A = A.filter(num => num!==maxNumA)
+                B = B.filter(num => num!==maxNumB)
+            } else {
+                return (maxNumA > maxNumB ? "Player A Wins" : "Player B Wins")
+            }
+        }
+    }
+
+    var convertToIntegers = (A) => {
+        var list = []
         for (i=0; i<A.length; i++) {
-            numberListA.push(values[A[i].slice(0, -1)])
+            list.push(values[A[i].slice(0, -1)])
         }
-        for (j=0; j<B.length; j++) {
-            numberListB.push(values[B[j].slice(0, -1)])
-        }
+        list.sort(function(a, b){return a - b})
+        return list
+    }
 
-        var maxNumberA = Math.max(...numberListA)
-        var maxNumberB = Math.max(...numberListB)
+    var convertToSuits = (A) => {
+        var list = []
+        for (i=0; i<A.length; i++) {
+            list.push(A[i].slice(-1))
+        } return list
+    }
 
-        if (maxNumberA > maxNumberB) {
-            return "Player A Wins"
-        }
-        if (maxNumberA < maxNumberB) {
-            return "Player B Wins"
-        }
-        else { return "Draw"
+    var checkSamePairs  = (A,B) => {
+        if ((A[0]===B[0] && A[1]===B[1])) {
+            return true
+        } 
+        else {
+            return false
         }
     }
 
@@ -41,15 +234,9 @@ function PokerGame() {
         1: 1
 
     }
-      
 
     return {deal}
-
-    
 }
 
-module.exports = PokerGame();
 
-// function highCard(maxNumberA, maxNumberB) {
-    //return maxNumberA > maxNumberB ? "Player A Wins" : "Player B Wins" || "Draw"
-// }
+module.exports = PokerGame();
--- katapoker/test/tests.js
+++ katapoker/test/tests.js
@@ -3,21 +3,86 @@ var expect = require("chai").expect
 
 describe( "PokerTests", function(){
 
-    it("Should Return Player B Wins when Player B has a picture card", function(){
-        expect(PokerGame.deal(["8D"], ["QS"])).to.equal("Player B Wins")
+    it("Should Return Player B wins when Comparing five cards with Player A", function(){
+        expect(PokerGame.deal(["KH", "2S", "3C", "1D", "10S"], ["KD", "8C", "9H", "1C", "10D"])).to.equal("Player B Wins")
     })
 
-    it("Should Return Draw if Player A and Player B Have Same Card", function(){
-        expect(PokerGame.deal(["10D"], ["10S"])).to.equal("Draw")
+    it("Should Return Draw when Comparing same five cards with Player B", function(){
+        expect(PokerGame.deal(["KH", "QS", "AC", "1D", "10S"], ["KD", "QH", "AS", "1C", "10D"])).to.equal("Draw")
     })
 
-    it("Should Return Player A wins when Comparing five cards with Player B", function(){
-        expect(PokerGame.deal(["KH", "QS", "AC", "1D", "10S"], ["2S", "1C", "5D", "6C", "KD"])).to.equal("Player A Wins")
+    it("Should Return Player B wins if Player B has pair", function(){
+        expect(PokerGame.deal(["2S", "1C", "AD", "6C", "KD"], ["KH", "KS", "AC", "1C", "10S"])).to.equal("Player B Wins")
     })
 
-    it("Should Return Draw when Comparing five cards with same High Card", function(){
-        expect(PokerGame.deal(["KH", "QS", "AC", "1D", "10S"], ["2S", "1C", "AD", "6C", "KD"])).to.equal("Draw")
+    it("Should Return Player A wins if Player A has the highest pair", function(){
+        expect(PokerGame.deal(["KH", "KS", "AC", "1C", "10S"], ["2S", "1C", "AD", "2H", "KD"])).to.equal("Player A Wins")
     })
+
+    it("Should Return Player B wins if both players have same pair but B has high card", function(){
+        expect(PokerGame.deal(["KC", "1C", "3D", "2H", "KD"], ["2C", "KH", "KS", "1C", "10S"])).to.equal("Player B Wins")
+    })
+
+    it("Should Return Draw if both players have same pair and same remaining cards", function(){
+        expect(PokerGame.deal(["KC", "KD", "1D", "2H","3D"], ["3C", "KH", "KS", "1C", "2S"])).to.equal("Draw")
+    })
+
+    it("Should Return Player A wins if A has double pair", function(){
+        expect(PokerGame.deal(["KH", "KS", "2C", "10C", "10S"], ["KC", "1C", "3D", "2H", "KD"])).to.equal("Player A Wins")
+    })
+
+    it("Should Return Player B wins if Player B has highest double pair", function(){
+        expect(PokerGame.deal(["KC", "1C", "1S", "3H", "3D"], ["2C", "KH", "KS", "10C", "10S"])).to.equal("Player B Wins")
+    })
+
+    it("Should Return Player A wins if both players have same double pair and A has high card", function(){
+        expect(PokerGame.deal(["10H", "10D", "3D", "KC", "KD"], ["2C", "KH", "KS", "10C", "10S"])).to.equal("Player A Wins")
+    })
+    
+    it("Should Return Player B wins if Player B has a highest Three Of a Kind", function(){
+        expect(PokerGame.deal(["2S", "6H", "AD", "6C", "6D"], ["KH", "1C", "KS", "10S", "KC"])).to.equal("Player B Wins")
+    })
+
+    it("Should Return Player A Wins if A has a Straight", function(){
+        expect(PokerGame.deal(["2S", "3H", "4D", "5C", "6D"], ["KH", "1C", "KS", "10S", "KC"])).to.equal("Player A Wins")
+    })
+
+    it("Should Return Player B Wins if B has highest Straight", function(){
+        expect(PokerGame.deal(["2S", "3H", "4D", "5C", "6D"], ["9H", "10C", "JS", "QS", "KC"])).to.equal("Player B Wins")
+    })
+
+    it("Should Return Player B Wins if B has a Flush", function() {
+        expect(PokerGame.deal(["2S", "3S", "4D", "5C", "6D"], ["9H", "10H", "5H", "7H", "AH"])).to.equal("Player B Wins")
+    })
+
+    it("Should Return Player A Wins if both players have flush but A has high card", function() {
+        expect(PokerGame.deal(["9H", "10H", "5H", "7H", "AH"], ["2S", "3S", "6S", "5S", "8S"])).to.equal("Player A Wins")
+    })
+
+    it("Should Return Player A Wins if A has Full House", function() {
+        expect(PokerGame.deal(["10S", "10H", "10D", "7H", "7S"], ["2S", "3S", "6S", "5S", "8S"])).to.equal("Player A Wins")
+    })
+
+    it("Should Return Player B Wins if both players have a Full House but B has higher Three of A Kind", function() {
+        expect(PokerGame.deal(["10S", "10H", "10D", "7H", "7S"], ["AS", "AC", "AH", "5S", "5D"])).to.equal("Player B Wins")
+    })
+
+    it("Should Return Player A Wins if A has Four of A Kind", function() {
+        expect(PokerGame.deal(["10S", "10H", "10D", "10C", "7S"], ["AS", "AC", "AH", "5S", "5D"])).to.equal("Player A Wins")
+    })
+
+    it("Should Return Player A Wins if both players have Four Of A Kind but A has highest", function() {
+        expect(PokerGame.deal(["10S", "10H", "10D", "10C", "7S"], ["2S", "2C", "2H", "2D", "5D"])).to.equal("Player A Wins")
+    })
+
+    it("Should Return Player B Wins if B has a Straight Flush", function() {
+        expect(PokerGame.deal(["10S", "10H", "10D", "10C", "7S"], ["2S", "3S", "4S", "5S", "6S"])).to.equal("Player B Wins")
+    })
+
+    it("Should Return Player A Wins if A has highest Straight Flush", function() {
+        expect(PokerGame.deal(["9H", "10H", "JH", "QH", "KH"], ["2S", "3S", "4S", "5S", "6S"])).to.equal("Player A Wins")
+    })
+
 }
 
 )
